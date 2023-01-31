f2ac02260a28d786d6b48ac4ba4cf3be4dd7cd7c
I am Alaska <stephen.alaska94@hotmail.com>
2023-01-31 09:51:58 +0000

    disable desk-list css

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const humburger = document.querySelector(".humburger");
-const Desk_nav = document.querySelector(".Desk_nav");
+// const humburger = document.querySelector(".humburger");
+// const Desk_nav = document.querySelector(".Desk_nav");
 
-humburger.addEventListener("click", () => {
-  humburger.classList.toggle("active");
-  Desk_nav.classList.toggle("active");
-});
+// humburger.addEventListener("click", () => {
+//   humburger.classList.toggle("active");
+//   Desk_nav.classList.toggle("active");
+// });
 
-document.querySelectorAll(".Desk_list").forEach((n) =>
-  n.addEventListener("click", () => {
-    humburger.classList.remove("active");
-    Desk_nav.classList.remove("active");
-  })
-);
+// document.querySelectorAll(".Desk_list").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     humburger.classList.remove("active");
+//     Desk_nav.classList.remove("active");
+//   })
+// );
